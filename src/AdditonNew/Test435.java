0c8a40727e1480e40187dda239aeeac99cded622
NACHIKETA <NACHIKETA@DESKTOP-9VGV2VS>
2023-02-03 22:18:26 +0530

    new commit to BranchPupun

--- src/AdditonNew/Test435.java
+++ src/AdditonNew/Test435.java
@@ -3,8 +3,7 @@ package AdditonNew;
 public class Test435 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Second Branch Add");
 	}
 
 }
--- src/AdditonNew/TestMehod.java
+++ src/AdditonNew/TestMehod.java
@@ -1,5 +1,8 @@
 package AdditonNew;
 
 public class TestMehod {
-
+public static void main(String[] args)
+{
+	System.out.println("first Branch Add");
+}
 }
